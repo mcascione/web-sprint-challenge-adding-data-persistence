6b8dc63f402156c1e2d6f3afe4a35654b24f95c4
Miranda Cascione <miranda.cascione@gmail.com>
2023-05-11 15:16:56 -0700

    started up basic server in index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// start your server here
+const express = require('express');
+
+require('dotenv').config();
+
+const server = express();
+
+const port = process.env.PORT || 9000;
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+})
