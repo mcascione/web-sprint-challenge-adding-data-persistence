a694154dfa43a9e5159b8978fd0b5b0920463827
Miranda Cascione <miranda.cascione@gmail.com>
2023-05-11 15:49:04 -0700

    completed projects table schema

---
+++ data/migrations/20230511223842_createTables.js
@@ -0,0 +1,35 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema
+
+    .createTable("projects", (tbl) => {
+      tbl.increments("project_id");
+      tbl.string("project_name").notNullable();
+      tbl.string("project_description");
+      tbl.boolean("project_completed").defaultTo(false);
+    })
+    .createTable("resources", (tbl) => {
+      tbl.increments("resource_id");
+    })
+    .createTable("tasks", (tbl) => {
+      tbl.increments("task_id");
+    })
+    .createTable("project_resources", (tbl) => {
+      tbl.increments("project_resource_id");
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("project_resources")
+    .dropTableIfExists("tasks")
+    .dropTableIfExists("resources")
+    .dropTableIfExists("projects");
+};
