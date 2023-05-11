49d548af464487800a42b28f744b4378c0293b12
Miranda Cascione <miranda.cascione@gmail.com>
2023-05-11 16:11:49 -0700

    rewound accidental delete of dbConfig

--- data/migrations/20230511223842_createTables.js
+++ data/migrations/20230511223842_createTables.js
@@ -6,31 +6,48 @@ exports.up = function (knex) {
   return knex.schema
 
     .createTable("projects", (tbl) => {
-      tbl.increments("project_id")
+      tbl.increments("project_id");
       tbl.string("project_name").notNullable();
-      tbl.string("project_description")
+      tbl.string("project_description");
       tbl.boolean("project_completed").defaultTo(false);
     })
     .createTable("resources", (tbl) => {
-      tbl.increments("resource_id")
+      tbl.increments("resource_id");
       tbl.string("resource_name").unique().notNullable();
-      tbl.string("resource_description")
+      tbl.string("resource_description");
     })
     .createTable("tasks", (tbl) => {
-      tbl.increments("task_id")
-      tbl.string('task_description').notNullable();
-      tbl.string('task_notes')
-      tbl.boolean('task_completed').defaultTo(false);
-      tbl.integer('project_id')
+      tbl.increments("task_id");
+      tbl.string("task_description").notNullable();
+      tbl.string("task_notes");
+      tbl.boolean("task_completed").defaultTo(false);
+      tbl
+        .integer("project_id")
         .unsigned()
         .notNullable()
-        .references('project_id')
-        .inTable('projects')
-        .onDelete('RESTRICT')
-        .onUpdate('RESTRICT')
+        .references("project_id")
+        .inTable("projects")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
     })
     .createTable("project_resources", (tbl) => {
       tbl.increments("project_resource_id");
+      tbl
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("project_id")
+        .inTable("projects")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+      tbl
+        .integer("resource_id")
+        .unsigned()
+        .notNullable()
+        .references("resource_id")
+        .inTable("resources")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
     });
 };
 
