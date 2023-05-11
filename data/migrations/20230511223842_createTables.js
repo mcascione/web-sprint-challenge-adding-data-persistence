845dcdeec8eaec0b6b46b017af0716bad699bf96
Miranda Cascione <miranda.cascione@gmail.com>
2023-05-11 16:00:17 -0700

    formed resources schema

--- data/migrations/20230511223842_createTables.js
+++ data/migrations/20230511223842_createTables.js
@@ -6,13 +6,15 @@ exports.up = function (knex) {
   return knex.schema
 
     .createTable("projects", (tbl) => {
-      tbl.increments("project_id");
+      tbl.increments("project_id")
       tbl.string("project_name").notNullable();
-      tbl.string("project_description");
+      tbl.string("project_description")
       tbl.boolean("project_completed").defaultTo(false);
     })
     .createTable("resources", (tbl) => {
-      tbl.increments("resource_id");
+      tbl.increments("resource_id")
+      tbl.string("resource_name").unique().notNullable();
+      tbl.string("resource_description")
     })
     .createTable("tasks", (tbl) => {
       tbl.increments("task_id");
