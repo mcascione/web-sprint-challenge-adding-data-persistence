aa2ed96188f4a129a5631bcda1f4329526bc3daf
Miranda Cascione <miranda.cascione@gmail.com>
2023-05-11 18:01:08 -0700

    completed get method for tasks

--- api/task/model.js
+++ api/task/model.js
@@ -1,5 +1,16 @@
-const db = require('../../data/dbConfig');
+const db = require("../../data/dbConfig");
+
+function FindTasks() {
+  return db("tasks as t")
+    .leftJoin("projects as p", "t.project_id", "p.project_id")
+    .select("t.*", "project_name", "project_description");
+}
+
+/* select t.*, project_name, project_description from tasks as t
+    left join projects as p
+        on t.project_id = p.project_id
+        */
 
 module.exports = {
-
+  FindTasks,
 };
--- api/task/router.js
+++ api/task/router.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const Task = require("./model");
 const router = express.Router();
 
-//eslint-disable-next-line
-router.get("/", (req, res, next) => {
-  res.status(200).json({ message: "hello from tasks router" });
+router.get("/", async (req, res, next) => {
+  try {
+    const tasks = await Task.FindTasks();
+    const formattedTasks = tasks.map((task) => ({
+      ...task,
+      task_completed: task.task_completed === 1 ? true : false,
+    }));
+    res.status(200).json(formattedTasks);
+  } catch (err) {
+    next(err);
+  }
 });
 
 //eslint-disable-next-line
